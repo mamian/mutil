2b952463b6d176210783c1155ed1b0c7ba84313b
mamian <mamianskyma@aliyun.com>
2016-12-14 23:40:30 +0800

    FileUtil

---
+++ src/main/java/net/mamian/util/FileUtil.java
@@ -0,0 +1,20 @@
+package net.mamian.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+
+/**
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2016-12-7 19:32:28
+ * @copyright ©2016 马面 All Rights Reserved
+ */
+public class FileUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(FileUtil.class);
+
+    
+
+}
