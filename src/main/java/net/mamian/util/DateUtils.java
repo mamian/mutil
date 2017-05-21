13f23a3be629d9b3bb7abd79a8778b004fc09b40
mamian <mamianskyma@aliyun.com>
2017-05-21 21:24:37 +0800

    计算农历某月的天数

--- src/main/java/net/mamian/util/DateUtils.java
+++ src/main/java/net/mamian/util/DateUtils.java
@@ -291,6 +291,20 @@ public class DateUtils {
     };
 
     /**
+     * 传回农历 year年month月的总天数，总共有13个月包括闰月
+     *
+     * @param year  将要计算的年份
+     * @param month 将要计算的月份
+     * @return 传回农历 year年month月的总天数
+     */
+    private static int daysInLunarMonth(int year, int month) {
+        if ((LUNAR_INFO[year - MIN_YEAR] & (0x100000 >> month)) == 0)
+            return 29;
+        else
+            return 30;
+    }
+
+    /**
      * 传回农历 year年闰哪个月 1-12 , 没闰传回 0
      *
      * @param year 将要计算的年份
