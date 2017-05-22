7cfb93e24142c67961fe204499b4b63af066445c
mamian <mamianskyma@aliyun.com>
2017-05-22 21:46:47 +0800

    农历某年总天数

--- src/main/java/net/mamian/util/DateUtils.java
+++ src/main/java/net/mamian/util/DateUtils.java
@@ -291,6 +291,25 @@ public class DateUtils {
     };
 
     /**
+     * 传回农历 year年的总天数
+     *
+     * @param year 将要计算的年份
+     * @return 返回传入年份的总天数
+     */
+    private static int daysInLunarYear(int year) {
+        int i, sum = 348;
+        if (leapMonthOfLunar(year) != 0) {
+            sum = 377;
+        }
+        int monthInfo = LUNAR_INFO[year - MIN_YEAR] & 0x0FFF80;
+        for (i = 0x80000; i > 0x7; i >>= 1) {
+            if ((monthInfo & i) != 0)
+                sum += 1;
+        }
+        return sum;
+    }
+
+    /**
      * 传回农历 year年month月的总天数，总共有13个月包括闰月
      *
      * @param year  将要计算的年份
