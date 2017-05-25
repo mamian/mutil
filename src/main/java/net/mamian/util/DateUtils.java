c517539d2b5df09bd1000b0f9f866de7bac2ef85
mamian <mamianskyma@aliyun.com>
2017-05-26 01:03:54 +0800

    农历转阳历

--- src/main/java/net/mamian/util/DateUtils.java
+++ src/main/java/net/mamian/util/DateUtils.java
@@ -350,6 +350,99 @@ public class DateUtils {
     }
 
     /**
+     * 将农历日期转换为公历日期
+     * @param year                        农历年份
+     * @param month                        农历月
+     * @param monthDay                农历日
+     * @param isLeapMonth        该月是否是闰月
+     * [url=home.php?mod=space&uid=7300]@return[/url] 返回农历日期对应的公历日期，year0, month1, day2.
+     */
+    public static final int[] lunarToSolar(int year, int month, int monthDay,
+                                           boolean isLeapMonth) {
+        int dayOffset;
+        int leapMonth;
+        int i;
+
+        if (year < MIN_YEAR || year > MAX_YEAR || month < 1 || month > 12
+                || monthDay < 1 || monthDay > 30) {
+            throw new IllegalArgumentException(
+                    "Illegal lunar date, must be like that:\n\t" +
+                            "year : 1900~2099\n\t" +
+                            "month : 1~12\n\t" +
+                            "day : 1~30");
+        }
+
+        dayOffset = (LUNAR_INFO[year - MIN_YEAR] & 0x001F) - 1;
+
+        if (((LUNAR_INFO[year - MIN_YEAR] & 0x0060) >> 5) == 2)
+            dayOffset += 31;
+
+        for (i = 1; i < month; i++) {
+            if ((LUNAR_INFO[year - MIN_YEAR] & (0x80000 >> (i - 1))) == 0)
+                dayOffset += 29;
+            else
+                dayOffset += 30;
+        }
+
+        dayOffset += monthDay;
+        leapMonth = (LUNAR_INFO[year - MIN_YEAR] & 0xf00000) >> 20;
+
+        // 这一年有闰月
+        if (leapMonth != 0) {
+            if (month > leapMonth || (month == leapMonth && isLeapMonth)) {
+                if ((LUNAR_INFO[year - MIN_YEAR] & (0x80000 >> (month - 1))) == 0)
+                    dayOffset += 29;
+                else
+                    dayOffset += 30;
+            }
+        }
+
+        if (dayOffset > 366 || (year % 4 != 0 && dayOffset > 365)) {
+            year += 1;
+            if (year % 4 == 1)
+                dayOffset -= 366;
+            else
+                dayOffset -= 365;
+        }
+
+        int[] solarInfo = new int[3];
+        for (i = 1; i < 13; i++) {
+            int iPos = DAYS_BEFORE_MONTH[i];
+            if (year % 4 == 0 && i > 2) {
+                iPos += 1;
+            }
+
+            if (year % 4 == 0 && i == 2 && iPos + 1 == dayOffset) {
+                solarInfo[1] = i;
+                solarInfo[2] = dayOffset - 31;
+                break;
+            }
+
+            if (iPos >= dayOffset) {
+                solarInfo[1] = i;
+                iPos = DAYS_BEFORE_MONTH[i - 1];
+                if (year % 4 == 0 && i > 2) {
+                    iPos += 1;
+                }
+                if (dayOffset > iPos)
+                    solarInfo[2] = dayOffset - iPos;
+                else if (dayOffset == iPos) {
+                    if (year % 4 == 0 && i == 2)
+                        solarInfo[2] = DAYS_BEFORE_MONTH[i] - DAYS_BEFORE_MONTH[i - 1] + 1;
+                    else
+                        solarInfo[2] = DAYS_BEFORE_MONTH[i] - DAYS_BEFORE_MONTH[i - 1];
+
+                } else
+                    solarInfo[2] = dayOffset;
+                break;
+            }
+        }
+        solarInfo[0] = year;
+
+        return solarInfo;
+    }
+
+    /**
      * 传回农历year年month月的总天数
      * @param year 要计算的年份
      * @param month        要计算的月
