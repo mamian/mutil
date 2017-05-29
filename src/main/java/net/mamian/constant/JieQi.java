a3b27a4a960591b8635a9db8510663b634806f4e
mamian <mamianskyma@aliyun.com>
2017-05-29 21:11:34 +0800

    节气

---
+++ src/main/java/net/mamian/constant/JieQi.java
@@ -0,0 +1,134 @@
+package net.mamian.constant;
+
+/**
+ * 24节气
+ *
+ * @author mamian
+ * @mail mamianskyma@aliyun.com
+ * @date 2017-05-29 21:01
+ * @copyright ©2017 马面 All Rights Reserved
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+public interface JieQi {
+
+    /**
+     * 立春
+     * */
+    public static int lichun=1;
+
+    /**
+     * 雨水
+     * */
+    public static int yushui=2;
+
+    /**
+     * 惊蛰
+     * */
+    public static int jingzhe=3;
+
+    /**
+     * 春分
+     * */
+    public static int chunfen=4;
+
+    /**
+     * 清明
+     * */
+    public static int qingming=5;
+
+    /**
+     * 谷雨
+     * */
+    public static int guyu=6;
+
+    /**
+     * 立夏
+     * */
+    public static int lixia=7;
+
+    /**
+     * 小满
+     * */
+    public static int xiaoman=8;
+
+    /**
+     * 芒种
+     * */
+    public static int mangzhong=9;
+
+    /**
+     * 夏至
+     * */
+    public static int xiazhi=10;
+
+    /**
+     * 小暑
+     * */
+    public static int xiaoshu=11;
+
+    /**
+     * 大暑
+     * */
+    public static int dashu=12;
+
+    /**
+     * 立秋
+     * */
+    public static int liqiu=13;
+
+    /**
+     * 处暑
+     * */
+    public static int chushu=14;
+
+    /**
+     * 白露
+     * */
+    public static int bailu=15;
+
+    /**
+     * 秋分
+     * */
+    public static int qiufen=16;
+
+    /**
+     * 寒露
+     * */
+    public static int hanlu=17;
+
+    /**
+     * 霜降
+     * */
+    public static int shuangjiang=18;
+
+    /**
+     * 立冬
+     * */
+    public static int lidong=19;
+
+    /**
+     * 小雪
+     * */
+    public static int xiaoxue=20;
+
+    /**
+     * 大雪
+     * */
+    public static int daxue=21;
+
+    /**
+     * 冬至
+     * */
+    public static int dongzhi=22;
+
+    /**
+     * 小寒
+     * */
+    public static int xiaohan=23;
+
+    /**
+     * 大寒
+     * */
+    public static int dahan=24;
+
+}
